4491f6df3b5d78cc2947ca0b97cc79bde8f91b53
Bilaal Ahmad <bilaal-ahmad@hotmail.co.uk>
2021-12-26 19:59:36 +0000

    AddToBooklistModal - Fixed unique key prop warning

--- client/src/components/AddToBooklistModal/AddToBooklistModal.jsx
+++ client/src/components/AddToBooklistModal/AddToBooklistModal.jsx
@@ -6,6 +6,25 @@ import { MasterListContext } from "contexts/master-list/master-list.context";
 // Components
 import PopupModal from "components/PopupModal/PopupModal";
 
+// --- AddToBooklistModal
+/**       
+ * Will be reused in:
+    - BookCard (to add book to an existing list) -> Create openModal state in PARENT: BookGrid 
+    - BookListPage (to add book from current list-in-view to another list)
+ 
+ * Create:
+ * Search box - used to filter list
+ * List of BookListItems -> with onClick -> triggering add to list
+    - add to list 
+        -> request to server, add bookId to a bookList array in DB
+        -> also adds book to state
+ 
+ * Context is required to track state of BookLists in app
+ * Thus allowing use of synced state across multiple components
+     - PopupModal, MasterListPage / BookListPage
+
+ * SIDE-NOTE: could even create a new AddToList component, containing the add button and PopupModal
+ */
 const AddToBooklistModal = ({ openModal, setOpenModal }) => {
     const masterList = useContext(MasterListContext);
     console.log("MODAL", masterList);
@@ -13,6 +32,8 @@ const AddToBooklistModal = ({ openModal, setOpenModal }) => {
     return (
         <PopupModal
             title="Add Book To A List"
+            // title="Add Book to Booklist"
+            // title="Save To List"
             openModal={openModal}
             setOpenModal={setOpenModal}
         >
@@ -24,8 +45,8 @@ const AddToBooklistModal = ({ openModal, setOpenModal }) => {
                     /* if (input.length) {} */
                     return true;
                 })
-                .map((booklist) => (
-                    <div>
+                .map((booklist, index) => (
+                    <div key={index}>
                         <p>Title: {booklist.title}</p>
                         <ul>
                             <li>
