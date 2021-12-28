589dadf0014f7721638d5b3d2eb4c9261bec74db
Bilaal Ahmad <bilaal-ahmad@hotmail.co.uk>
2021-12-28 20:04:07 +0000

    Updated Booklist model; accomodate user relationship, added schema validation and additional properties

--- server/models/Booklist.js
+++ server/models/Booklist.js
@@ -1,14 +1,25 @@
 import mongoose from "mongoose";
 
+/**
+ * One-to-many - Data Modelling
+ * Embedding - https://docs.mongodb.com/manual/tutorial/model-embedded-one-to-many-relationships-between-documents/
+ * Referencing (or Linking) - https://docs.mongodb.com/manual/tutorial/model-referenced-one-to-many-relationships-between-documents/
+ */
 const booklistSchema = new mongoose.Schema(
     {
+        // reference to associated user
+        userId: {
+            type: mongoose.Types.ObjectId,
+            required: [true, "A booklist must have an associated user"],
+        },
         title: {
             type: String,
-            required: true,
+            required: [true, "A Booklist title is required"],
         },
         description: String,
         // Primitive Array - https://mongoosejs.com/docs/schematypes.html#arrays
         bookIds: [String],
+        bookCount: Number,
     },
     { timestamps: true }
 );
