df61b6f67205c9c209632dbf8193f8897d282314
Bilaal Ahmad <bilaal-ahmad@hotmail.co.uk>
2022-01-15 17:07:47 +0000

    Updated MUI Theme Primary Colours

--- client/src/config/mui-theme.js
+++ client/src/config/mui-theme.js
@@ -3,7 +3,11 @@ import teal from "@material-ui/core/colors/teal";
 
 const theme = createTheme({
     palette: {
-        primary: teal,
+        primary: {
+            light: teal[500],
+            main: teal[600],
+            dark: teal[800],
+        },
         secondary: {
             // https://www.rapidtables.com/web/color/purple-color.html
             main: "#8B008B",
